d4a130a4af9a0db43e299954e3fdef8ea26298f0
Stefan <stefantudorache90@yahoo.com>
2020-11-20 17:32:10 +0200

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>This is The Test</p>
+        <h1>SECOND TEST</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
