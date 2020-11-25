e4c29b1c83bd3c6b20bbf429a577b74169357cac
Ciprian <48722279+Ciprian-A@users.noreply.github.com>
2020-11-25 15:47:43 +0000

    re-edited the feature/logo branch

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./Logo.module.scss";
-import splash from '../../assets/images/splash.png';
-import myLogo from '../../assets/images/MY.png';
+import personLogo from '../../assets/images/person-logo.png';
+import roundLogo from '../../assets/images/round-logo.png';
 
 const Logo = () => {
   return (
     <div className={styles.logoContainer}>
-      <img src={myLogo} alt='round img' className={styles.roundImg}/>
+      <img src={roundLogo} alt='happy app round logo on a yellow background' className={styles.roundLogo}/>
       <h2 className={styles.textLogo}>Happy App</h2>
-      <img src={splash} alt='splash img' className={styles.splash}/>
+      <img src={personLogo} alt='happy person logo' className={styles.personLogo}/>
     </div>
   );
 };
