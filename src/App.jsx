ae2537e091ceb487cc79b8fe1da8acf3d07e55c4
josh-foster <joshfoster321@hotmail.com>
2020-11-25 16:25:59 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 import styles from "./App.module.scss";
 import LandingPageButton from "./components/LandingPageButton";
 
 
 
+
+
 const App = () => {
 
   return (
-  <> 
+  
   <div className={styles.LandingPageButtons}>
     <LandingPageButton text={"Sign Up"} cardType="primary"/>
     <LandingPageButton text={"Log In"} cardType="secondary"/>
   </div>
-  </>
-
-
+  
 
   );
 }
 
-export default App;
+export default App; 
