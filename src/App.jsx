a42340c54b22d242ac3c6eeefd36404805215906
Tym Simonson <59534146+ttytango@users.noreply.github.com>
2020-11-26 16:59:38 +0000

    update spelling
    Updated spelling of Suggestion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Sugestion from "./components/Suggestion"
+import Suggestion from "./components/Suggestion"
 import Logo from './components/Logo';
 import styles from "./App.module.scss";
 import LandingPageButton from "./components/LandingPageButton";
@@ -13,7 +13,7 @@ const App = () => {
     <div className={styles.body}>
     
     <Navbar text="Happiness Scores"/>
-      <Sugestion text="Relations with your parents, siblings, spouse, close friends. " isClicked=  {true}/>
+      <Suggestion text="Relations with your parents, siblings, spouse, close friends. " isClicked=  {true}/>
       <Logo />  
       <div className={styles.LandingPageButtons}>
         <LandingPageButton text={"Sign Up"} cardType="primary"/>
