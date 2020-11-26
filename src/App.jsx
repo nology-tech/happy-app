b84ebdd3c4cefd542e6f7a160d097cf3461ca1e6
Ciprian <48722279+Ciprian-A@users.noreply.github.com>
2020-11-26 17:22:02 +0000

    imported TaskInput to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
 import './App.css';
-<<<<<<< HEAD
 import Suggestion from "./components/Suggestion"
 import Logo from './components/Logo';
 import styles from "./App.module.scss";
 import LandingPageButton from "./components/LandingPageButton";
-=======
-import Sugestion from "./components/Suggestion"
-import Logo from './components/Logo';
-import styles from "./App.module.scss";
-import LandingPageButton from "./components/LandingPageButton";
-import Navbar from "./components/Navbar"
-import TaskInput from "./components/TaskInput";
+import TaskInput from './components/TaskInput';
 
->>>>>>> 034c30b59084c229bfe05f59a00cb5272a16e477
 const App = () => {
   return (
-<<<<<<< HEAD
-    <>
+    <div>
       <Suggestion text="Relations with your parents, siblings, spouse, close friends. " isClicked=  {true}/>
-=======
-    <div className={styles.body}>
-    
-      <Navbar text="Happiness Scores"/>
-      <Sugestion text="Relations with your parents, siblings, spouse, close friends. " isClicked=  {true}/>
->>>>>>> 034c30b59084c229bfe05f59a00cb5272a16e477
       <Logo />  
       <div className={styles.LandingPageButtons}>
         <LandingPageButton text={"Sign Up"} cardType="primary"/>
         <LandingPageButton text={"Log In"} cardType="secondary"/>
       </div>
-      <TaskInput/>
+      <TaskInput />
     
     </div>
   );
