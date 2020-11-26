0f7ec4923863e5ee4d35a097be67081fe795df16
Sachin <sachin.patel9966@gmail.com>
2020-11-26 16:08:43 +0000

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import './App.css';
 import Sugestion from "./Components/Suggestion"
-import Logo from './components/Logo';
+import Logo from './Components/Logo';
 import styles from "./App.module.scss";
-import LandingPageButton from "./components/LandingPageButton";
+import LandingPageButton from "./Components/LandingPageButton";
+import Navbar from './Components/Navbar/Navbar';
 
 
 
 const App = () => {
 
   return (
-    <>
+    <div className={styles.body}>
+    
+    <Navbar text="Happiness Scores"/>
       <Sugestion text="Relations with your parents, siblings, spouse, close friends. " isClicked=  {true}/>
       <Logo />  
       <div className={styles.LandingPageButtons}>
         <LandingPageButton text={"Sign Up"} cardType="primary"/>
         <LandingPageButton text={"Log In"} cardType="secondary"/>
       </div>
-    </>
+    </div>
   );
 };
 
