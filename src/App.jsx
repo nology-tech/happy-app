2518eab7edc0f57b88c4db88df7f7e63a2c75624
Matthew Bickell <matthew@nology.io>
2020-12-01 17:05:56 +0000

    minor change to get workflow file to run :beer:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from "./App.module.scss";
-
 // import LifeComponent from "./components/LifeComponent";
 import "./data/fa-library";
 import Suggestion from "./components/Suggestion"
