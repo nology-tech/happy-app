43b3c8bbda83f216b3b1801d09213e6ac38a53e1
Ezio Intravaia <ezio.intravaia@hotmail.com>
2020-12-07 17:17:44 +0000

    fixed navbar

--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -1,50 +1,47 @@
 import React from "react";
 import styles from "./NavItem.module.scss";
 import { Link } from "@reach/router";
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const NavItem = (props) => {
-  const { signOut } = props
+  const { setOpenMenu, signOut } = props
   return (
     <div className={styles.navBar}>
+      <span onClick={() => {
+        setOpenMenu(false)
+      }}>
+       <FontAwesomeIcon icon="times" className={styles.faIcon} />
+       </span>
       <ul className={styles.navList}>
-
         <Link to="/">
           <li>
             Home
           </li>
         </Link>  
-
         <Link to="scores">
           <li>
             My Scores
           </li>
         </Link>
-
         <Link to="task">
           <li>
             Tasks
           </li>
         </Link>
-        
         <Link to="accountsettings">
           <li>
             Account Settings
           </li>
         </Link>
-
         <Link to="about">
           <li>
             About
           </li>
         </Link>
-
         <Link to="termsconditions">
           <li>
             Terms and Conditions
           </li>
         </Link>
-
         <li className={styles.signOutButton} onClick={signOut}>
           Sign Out
         </li>
@@ -52,5 +49,4 @@ const NavItem = (props) => {
     </div>
   );
 };
-
 export default NavItem;
