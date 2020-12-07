e23b8bbcdaaf8eb6555d7a1613fec048b61a7f71
Steven Sanins <stevensanins@stevens-imac.home>
2020-12-07 11:11:19 +0000

    Changes to styling - not finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,43 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./App.module.scss";
 
 import "./data/fa-library";
 import Routes from "./containers/Routes";
 
-
-import firebase from './firebase';
+import firebase from "./firebase";
+import InitialScorePage from "./containers/InitialScorePage/InitialScorePage";
 
 // DONT ADD ANYTHING HERE - GO TO CONTAINERS AND RENDER STUFF THERE
 const App = () => {
-const [ user, setUser ] = useState(null);
+  const [user, setUser] = useState(null);
 
-// const signIn = () => {
-//     firebase.auth().signInWithRedirect(googleProvider);
-//   };
+  // const signIn = () => {
+  //     firebase.auth().signInWithRedirect(googleProvider);
+  //   };
 
-// const signOut = () => {
-//     firebase.auth().signOut();
-//   };
+  // const signOut = () => {
+  //     firebase.auth().signOut();
+  //   };
 
-const getUser = () => {
-  firebase.auth().onAuthStateChanged ((user) => {
-     if (user) {
-      setUser(user);
-    } else {
-      setUser(null);
-    }
-  });
-};
+  const getUser = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  };
 
-useEffect(() => {
-  getUser();
-}, []);
-  
-  
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div className={styles.body}>
-      <Routes user={user}/>
+      {/* <Routes user={user} /> */}
+      <InitialScorePage />
     </div>
-  ); 
+  );
 };
-export default App; 
+export default App;
