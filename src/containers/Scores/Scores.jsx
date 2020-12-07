671b5d865b121dffce3827187a86d96d81beee20
Otis Parsons <otis@Otiss-MacBook-Pro.local>
2020-12-07 13:07:36 +0000

    commit

--- src/containers/Scores/Scores.jsx
+++ src/containers/Scores/Scores.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./Scores.module.scss";
 
 const Scores = (props) => {
-  const { signOut } = props
   return (
     <>
       <p className={styles.text}></p>
