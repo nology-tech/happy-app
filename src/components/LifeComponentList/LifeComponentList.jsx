8a5e54af2e83cbc472abd3c8f73a31f780d5feec
wowermalwina <72358702+wowermalwina@users.noreply.github.com>
2020-12-16 18:28:21 +0000

    subheadings added to the score page :star:

--- src/components/LifeComponentList/LifeComponentList.jsx
+++ src/components/LifeComponentList/LifeComponentList.jsx
@@ -1,28 +1,56 @@
 import React from "react";
-// import styles from "./LifeComponentList.module.scss";
+import styles from "./LifeComponentList.module.scss";
 import LifeComponent from "../LifeComponent";
 
 const LifeComponentList = (props) => {
   const { scores, updateScore } = props;
 
-  const allLifeComponents = scores.map((component) => {
-    return (
-      <LifeComponent
-        rangeValue={component.score}
-        handleChange={(event) => updateScore(event, component.id)}
-        lifeComponentSuggestion={component.suggestion}
-        key={component.id}
-        lifeComponentNames={component.name}
-        icon={component.icon}
-      />
-    );
+  const practicalScores = scores.filter((score) => {
+    return score.category === "practical";
   });
+  const wellbeingScores = scores.filter((score) => {
+    return score.category === "wellbeing";
+  });
+  const relationshipsScores = scores.filter((score) => {
+    return score.category === "relationships";
+  });
+  const higherThoughtsScores = scores.filter((score) => {
+    return score.category === "higher-thoughts";
+  });
+  const activityScores = scores.filter((score) => {
+    return score.category === "activity";
+  });
+
+  const x = (newScores) => {
+    const showComponents = newScores.map((component) => {
+      return (
+        <LifeComponent
+          rangeValue={component.score}
+          handleChange={(event) => updateScore(event, component.id)}
+          lifeComponentSuggestion={component.suggestion}
+          key={component.id}
+          lifeComponentNames={component.name}
+          icon={component.icon}
+        />
+      );
+    });
+    return showComponents;
+  };
 
   return (
     <>
-   {allLifeComponents}
-   </>
- );
+      <h2 className={styles.subheading}>Wellbeing</h2>
+      {x(wellbeingScores)}
+      <h2 className={styles.subheading}>Relationships</h2>
+      {x(relationshipsScores)}
+      <h2 className={styles.subheading}>Activity</h2>
+      {x(activityScores)}
+      <h2 className={styles.subheading}>Practical</h2>
+      {x(practicalScores)}
+      <h2 className={styles.subheading}>Higher Thoughts</h2>
+      {x(higherThoughtsScores)}
+    </>
+  );
 };
 
 export default LifeComponentList;
--- src/data/data.js
+++ src/data/data.js
@@ -1,79 +1,113 @@
 const components = [
-  { id: 1,
+  {
+    id: 1,
     name: "Physical Environment",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 3
-},
-  { id: 2,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 3,
+    category: "practical",
+  },
+  {
+    id: 2,
     name: "Finances",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 5
-},
-  { id: 3,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 5,
+    category: "practical",
+  },
+  {
+    id: 3,
     name: "Career",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 4,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+    category: "practical",
+  },
+  {
+    id: 4,
     name: "General Health",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 5,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+    category: "wellbeing",
+  },
+  {
+    id: 5,
     name: "Mental Fitness",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 6,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 6,
     name: "Physical Fitness",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 7,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 7,
     name: "Friends",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 8,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 8,
     name: "Family",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 9,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 9,
     name: "Love life",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 10,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 10,
     name: "Spirituality",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 11,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 11,
     name: "Purpose",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 12,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 12,
     name: "Self Worth",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 13,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 13,
     name: "Fun And recreation",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 14,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 14,
     name: "PlaceholderOne",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-},
-  { id: 15,
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
+  {
+    id: 15,
     name: "PlaceholderTwo",
-    suggestion: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!',
-    score: 0
-}
+    suggestion:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    score: 0,
+  },
 ];
 
 export default components;
