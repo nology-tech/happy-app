6f19325e0128764315042b0452b2ed988947b8ac
josh-foster <joshfoster321@hotmail.com>
2020-12-17 14:47:53 +0000

    test issue fixed

--- src/containers/Task/Task.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Task from "./Task";
-
-describe("Task tests", () => {
-  it("should render", () => {
-    expect(render(<Task />)).toBeTruthy();
-  });
-});
