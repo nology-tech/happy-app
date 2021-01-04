90c4106c1639a721ca5ad946d9da13aca56c8984
Matthew Bickell <matthew@nology.io>
2021-01-04 18:31:02 +0000

    Added changeable title text to the suggestions

--- src/components/Suggestion/Suggestion.jsx
+++ src/components/Suggestion/Suggestion.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import styles from "./Suggestion.module.scss";
 
-
 const Suggestion = (props) => {
+  // Props = Data text that will change based on whicever life component you click on.
+  const { lifeComponentSuggestion, isClicked } = props;
 
-  
-    // Props = Data text that will change based on whicever life component you click on.
-  const {lifeComponentSuggestion, isClicked} = props;
-
-
-    //Add - Remove the Show class (display element) when clicking life component.
+  //Add - Remove the Show class (display element) when clicking life component.
   const display = isClicked ? styles.show : " ";
 
   return (
-  <article className={`${display} ${styles.message}`}>
-    <h3>Please rate this life component based on the following aspects:</h3>
-    <p>{lifeComponentSuggestion}</p>
-  </article>
+    <article className={`${display} ${styles.message}`}>
+      <h3>{lifeComponentSuggestion.title}</h3>
+      <p>{lifeComponentSuggestion.text}</p>
+    </article>
   );
 };
 
--- src/data/lifeComponents.js
+++ src/data/lifeComponents.js
@@ -2,8 +2,11 @@ const lifeComponents = [
   {
     id: 1,
     name: "Physical Environment",
-    suggestion:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    suggestion: {
+      title: "Please rate this life component based on the following aspects",
+      text:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!"
+    },
     score: 0,
     icon: "laptop-house",
     category: "practical"
@@ -11,8 +14,11 @@ const lifeComponents = [
   {
     id: 2,
     name: "Finances",
-    suggestion:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    suggestion: {
+      title: "Please rate this life component based on the following aspects",
+      text:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!"
+    },
     score: 0,
     icon: "donate",
     category: "practical"
@@ -20,8 +26,11 @@ const lifeComponents = [
   {
     id: 3,
     name: "Career",
-    suggestion:
-      "Are you in the job you want to be in? Are you advancing in your career? Are you paid what you think is fair? Do you have a plan to move forward in your career?",
+    suggestion: {
+      title: "How happy are you with your career at the moment?",
+      text:
+        "Are you in the job you want to be in? Are you advancing in your career? Are you paid what you think is fair? Do you have a plan to move forward in your career?"
+    },
     score: 0,
     icon: "user-tie",
     category: "practical"
@@ -29,8 +38,11 @@ const lifeComponents = [
   {
     id: 4,
     name: "General Happiness",
-    suggestion:
-      "How happy are you? How happy do you feel in a general context.  Don’t worry too much about details, or why, that will come later… but as a gut feel – how happy are you out of 10?",
+    suggestion: {
+      title: "Please rate your General Happiness out of 10",
+      text:
+        "How happy are you? How happy do you feel in a general context.  Don’t worry too much about details, or why, that will come later… but as a gut feel – how happy are you out of 10?"
+    },
     score: 0,
     icon: "smile",
     category: "wellbeing"
@@ -38,8 +50,11 @@ const lifeComponents = [
   {
     id: 5,
     name: "Mental Health",
-    suggestion:
-      "How happy are you with your Mental Health? Do you feel ok? Are you feeling positive or Low at the moment? Do you have any issues with stress, anxiety, depression?",
+    suggestion: {
+      title: "Please rate how you currently feel about your Mental Health out of 10",
+      text:
+        "How happy are you with your Mental Health? Do you feel ok? Are you feeling positive or Low at the moment? Do you have any issues with stress, anxiety, depression?"
+    },
     score: 0,
     icon: "brain",
     category: "wellbeing"
@@ -47,8 +62,12 @@ const lifeComponents = [
   {
     id: 6,
     name: "Physical Health",
-    suggestion:
-      "How are you feeling physically? Are you healthy? Are you fit? Are you in the physical shape you want to be in? Any aches and pains?",
+    suggestion: {
+      title:
+        "Please rate how happy you feel about your Physical Health 1 = totally unhappy. 10 = I could not be happier with my physical health",
+      text:
+        "How are you feeling physically? Are you healthy? Are you fit? Are you in the physical shape you want to be in? Any aches and pains?"
+    },
     score: 0,
     icon: "heartbeat",
     category: "wellbeing"
@@ -56,8 +75,11 @@ const lifeComponents = [
   {
     id: 7,
     name: "Friends",
-    suggestion:
-      "Do you have enough friends? Do you see them? Do you speak to them as much as you would like? Do they have a positive effect on your life? Would you like to see them more or maybe less? How are you getting on with your best friends?",
+    suggestion: {
+      title: "Please rate your happiness with regards to how you feel about your friendships out of 10",
+      text:
+        "Do you have enough friends? Do you see them? Do you speak to them as much as you would like? Do they have a positive effect on your life? Would you like to see them more or maybe less? How are you getting on with your best friends?"
+    },
     score: 0,
     icon: "user-friends",
     category: "relationships"
@@ -65,8 +87,12 @@ const lifeComponents = [
   {
     id: 8,
     name: "Family",
-    suggestion:
-      "Whether blood related, or related through love, how do you feel about the relationships with your family? Would you like to be closer? See more/less of each other? Maybe you have a great relationship with some family members and not others. Overall, how happy do you currently feel about your family relationships.",
+    suggestion: {
+      title:
+        "Family comes in many varieties – how happy do you feel about the relationships you have with the people you consider your family?",
+      text:
+        "Whether blood related, or related through love, how do you feel about the relationships with your family? Would you like to be closer? See more/less of each other? Maybe you have a great relationship with some family members and not others. Overall, how happy do you currently feel about your family relationships."
+    },
     score: 0,
     icon: "users",
     category: "relationships"
@@ -74,8 +100,11 @@ const lifeComponents = [
   {
     id: 9,
     name: "Love Life",
-    suggestion:
-      "Is your current Love Life set up working for you? Are you happy with it?– whether a special someone or even if you aren’t ready for that commitment – how happy are you with your relationship status, can you improve it? how? Do you want to make room for more romance?",
+    suggestion: {
+      title: "Aaah – Romance and Relationships… Love!.. How do you feel about yours?",
+      text:
+        "Is your current Love Life set up working for you? Are you happy with it?– whether a special someone or even if you aren’t ready for that commitment – how happy are you with your relationship status, can you improve it? how? Do you want to make room for more romance?"
+    },
     score: 0,
     icon: "heart",
     category: "relationships"
@@ -83,8 +112,11 @@ const lifeComponents = [
   {
     id: 10,
     name: "Spirituality",
-    suggestion:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    suggestion: {
+      title: "Please rate this life component based on the following aspects",
+      text:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!"
+    },
     score: 0,
     icon: "place-of-worship",
     category: "higher-thoughts"
@@ -92,8 +124,11 @@ const lifeComponents = [
   {
     id: 11,
     name: "Purpose",
-    suggestion:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    suggestion: {
+      title: "Please rate this life component based on the following aspects",
+      text:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!"
+    },
     score: 0,
     icon: "bullseye",
     category: "higher-thoughts"
@@ -101,8 +136,11 @@ const lifeComponents = [
   {
     id: 12,
     name: "Self Worth",
-    suggestion:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    suggestion: {
+      title: "Please rate this life component based on the following aspects",
+      text:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!"
+    },
     score: 0,
     icon: "hand-holding-heart",
     category: "higher-thoughts"
@@ -110,8 +148,12 @@ const lifeComponents = [
   {
     id: 13,
     name: "Fun and Recreation",
-    suggestion:
-      "What do you do in your free time? What do you want to do? What do you enjoy doing? Do you do it enough? Or do you need to do it less? How happy are you with what you do, and how much time you spend doing your hobbies/interests? Fancy trying something new?",
+    suggestion: {
+      title:
+        "This is all about the stuff you enjoy doing! How happy are you with the time you spend doing the things you love!",
+      text:
+        "What do you do in your free time? What do you want to do? What do you enjoy doing? Do you do it enough? Or do you need to do it less? How happy are you with what you do, and how much time you spend doing your hobbies/interests? Fancy trying something new?"
+    },
     score: 0,
     icon: "umbrella-beach",
     category: "activity"
@@ -119,8 +161,11 @@ const lifeComponents = [
   {
     id: 14,
     name: "Contribution to Society",
-    suggestion:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    suggestion: {
+      title: "Please rate this life component based on the following aspects",
+      text:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!"
+    },
     score: 0,
     icon: "hands-helping",
     category: "activity"
@@ -128,8 +173,11 @@ const lifeComponents = [
   {
     id: 15,
     name: "Personal Development",
-    suggestion:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!",
+    suggestion: {
+      title: "Please rate this life component based on the following aspects",
+      text:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam esse eum error impedit rem aliquid omnis. Deleniti possimus nobis vero!"
+    },
     score: 0,
     icon: "lightbulb",
     category: "activity"
